60693c7b5c4317d4e88abd76868974aa4163f905
Zaman <zamandev.us@gmail.com>
2023-09-07 06:36:41 -0700

    Heartland Payments System: - Coding challenge to organize and rename provided photos list - Create Photos class to handle grouping of input in cities and constructing new photo names list - Create Photo class to handle constructing new photo names and ordering based on input's order

---
+++ photo.rb
@@ -0,0 +1,19 @@
+class Photo
+  def self.construct_name(city_groups, city, photo, index)
+    max_length = city_groups[city].size.to_s.length
+    _, name = photo
+    natural_number = (index + 1).to_s.rjust(max_length, '0')
+
+    "#{city}#{natural_number}.#{name.split('.').last}"
+  end
+
+  def self.extract_names(city_groups)
+    new_list = city_groups.values.flat_map do |group|
+      group.map { |idx, name| [idx, name] }
+    end
+
+    new_list.sort_by!(&:first).map!(&:last)
+
+    new_list
+  end
+end
---
+++ photos.rb
@@ -0,0 +1,44 @@
+require('./photo')
+
+class Photos
+  attr_reader :photos
+
+  def initialize(list)
+    @photos = list.split("\n")
+  end
+
+  def organize
+    city_groups = group_by_city
+    city_groups = assign_numbers_and_rename(city_groups)
+
+    photo_names_list = Photo.extract_names(city_groups)
+    photo_names_list.join("\n")
+  end
+
+  def group_by_city
+    city_groups = Hash.new { |hash, key| hash[key] = [] }
+
+    photos.each_with_index do |photo, idx|
+      name, city, timestamp = photo.split(', ')
+
+      city_groups[city] << [idx, name, timestamp]
+    end
+
+    city_groups
+  end
+
+  def assign_numbers_and_rename(city_groups)
+    city_groups.each do |city, photo_list|
+      photo_list.sort_by! { |(_, _, timestamp)| timestamp }
+
+      photo_list.each_with_index do |photo, index|
+        idx, _, timestamp = photo
+        new_name = Photo.construct_name(city_groups, city, photo, index)
+
+        city_groups[city][index] = [idx, new_name, timestamp]
+      end
+    end
+
+    city_groups
+  end
+end
